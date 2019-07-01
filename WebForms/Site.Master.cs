ae1cc47a4a583e12c6e1ee8f5aca45d54a18252d
Juan Dausa <juandausa@gmail.com>
2019-06-30 23:59:18 -0300

    Se agrega el proyecto Web en ASP.NET incluyendo el uso de un extension method para detectar si el cliente está conectado.

---
+++ WebForms/Site.Master.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Web.UI;
+using WebForms.Utils;
+
+
+namespace WebForms
+{
+    public partial class SiteMaster : MasterPage
+    {
+        static int TotalRequest { get; set; }
+        static int TotalEfectiveRequest { get; set; }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            TotalRequest += 1;
+            if (!this.IsClientConnected())
+            {
+                Response.End();
+            }
+
+            TotalEfectiveRequest += 1;
+        }
+    }
+}
---
+++ WebForms/Utils/MasterPageExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.UI;
+
+namespace WebForms.Utils
+{
+    public static class MasterPageExtensions
+    {
+        public static bool IsClientConnected(this MasterPage masterPage)
+        {
+            return masterPage.Response.IsClientConnected;
+        }
+    }
+}
